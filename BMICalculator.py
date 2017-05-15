8cda5887b1be5ba865b85bf2b279a07f6c88cd46
Grimaldi_Madison <grimm192@hsd.k12.or.us>
2017-05-15 11:32:10 -0700

    adding successful code

--- BMICalculator.py
+++ BMICalculator.py
@@ -5,6 +5,12 @@
 # Define Function below
 # be sure to return an integer
 
+import math
+def calculateBMI(h, w):
+
+    BMI = w/h**2*703
+    BMI = round(BMI, 0)
+    return BMI
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
